10b1eebcf154ea5f987bb97443b8d5b1076d4b3e
Pablo Fanjul <pfanjul81@gmail.com>
2024-02-15 14:50:48 +0100

    added index route on index branch

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get "workspaces", to: "workspaces#index"
 
   root to: "pages#home"
 
