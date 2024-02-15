29d9c5a4fb8c2c4799cb3ddbb39ea136258ca85f
Pablo Fanjul <pfanjul81@gmail.com>
2024-02-15 20:02:27 +0100

    delete unnecessary migration files

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_02_15_132524) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_15_151839) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -22,13 +22,22 @@ ActiveRecord::Schema[7.1].define(version: 2024_02_15_132524) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "name"
+    t.boolean "is_owner"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   create_table "workspaces", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.string "location"
+    t.bigint "user_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_workspaces_on_user_id"
   end
 
+  add_foreign_key "workspaces", "users"
 end
