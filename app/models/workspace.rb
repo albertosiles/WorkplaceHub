a56d1726f4405ed33bf3939978d258fb4bab701f
Theresa Niedernhuber <theresa-niedernhuber@live.de>
2024-02-15 23:37:34 +0100

    review integrated

--- app/models/workspace.rb
+++ app/models/workspace.rb
@@ -1,5 +1,5 @@
 class Workspace < ApplicationRecord
   belongs_to :user
   has_many :bookings
-  has_many :reviews
+  has_many :reviews, dependent: :destroy
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 Rails.application.routes.draw do
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "workspaces", to: "workspaces#index"
   root to: "workspaces#index"
 
   resources :workspaces do
     resources :reviews, only: [:new, :create]
   end
+
+resources :reviews, only: [:destroy]
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
