d2d0ebe498c615352ab775376a877706db044f3f
Theresa Niedernhuber <theresa-niedernhuber@live.de>
2024-02-15 21:59:06 +0100

    seeds for workspaces created

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,10 @@
 #
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
+
+Workspace.create(name:"Fancy place", description: "big and beautiful and fancy", price: 100, location: "a nice city")
+Workspace.create(name:"Small place", description: "little sweet hut", price: 50, location: "a nice village")
+Workspace.create(name:"Professional place", description: "high-tech and rusty", price: 150, location: "the biggest city")
+Workspace.create(name:"Quirky place", description: "full of Batmans", price: 200, location: "Deadpool town")
+Workspace.create(name:"Pink place", description: "bling bling bling", price: 150, location: "Barbie town")
 #   end
