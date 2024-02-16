5391c82ab2936685c12024b8960375679ecce215
Theresa Niedernhuber <theresa-niedernhuber@live.de>
2024-02-16 02:06:20 +0100

    workspaces CRUD implemented

--- app/controllers/workspaces_controller.rb
+++ app/controllers/workspaces_controller.rb
@@ -1,5 +1,6 @@
 class WorkspacesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:index]
+
   def index
     @workspaces = Workspace.all
   end
@@ -9,14 +10,24 @@ class WorkspacesController < ApplicationController
   end
 
   def create
-    workspace = Workspace.new(workspace_params)
-    workspace.save
-    redirect_to workspace_path(workspace)
+    @workspace = Workspace.new(workspace_params)
+    @workspace.user = current_user
+    if @workspace.save
+      redirect_to workspace_path(@workspace)
+    else
+      render :new, status: :unprocessable_entity
     end
+  end
+
+  def destroy
+    @workspace = Workspace.find(params[:id])
+    @workspace.destroy
+    redirect_to workspaces_path
+  end
 
   private
 
   def workspace_params
-    params.require[:workspace].permit(:name, :description, :price, :location)
-    end
+    params.require(:workspace).permit(:name, :description, :price, :location)
+  end
 end
--- app/models/workspace.rb
+++ app/models/workspace.rb
@@ -2,4 +2,9 @@ class Workspace < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :reviews, dependent: :destroy
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :location, presence: true
 end
