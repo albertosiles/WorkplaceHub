7c38581008c88f99c1de6b67eecc27335bc3f2e1
alberto siles cayetano <alberto.siles.cayetano@gmail.com>
2024-02-17 13:40:23 +0000

    bookings controller added

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,42 @@
 class BookingsController < ApplicationController
+  # Ensure the user is authenticated before performing any action
+  before_action :authenticate_user!
+  # POST /bookings
+  def create
+    @booking = current_user.bookings.new(booking_params)
+
+    if @booking.save
+      redirect_to @booking, notice: 'Booking was successfully created.'
+    else
+      render :new
+    end
+  end
+
+  # PATCH/PUT /bookings/1
+  def update
+    @booking = Booking.find(params[:id])
+    if @booking.update(booking_params)
+      redirect_to @booking, notice: 'Booking was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  # DELETE /bookings/1
+  def destroy
+    @booking = Booking.find(params[:id])
+    @booking.destroy
+    redirect_to bookings_url, notice: 'Booking was successfully destroyed.'
+  end
+
+  private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def booking_params
+    params.require(:booking).permit(:id_user, :id_workspace, :start_date, :end_date)
+  end
 end
